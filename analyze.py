43c01aaf117ebdb06a2a432698349bf760a1c506
Evan Shockley <ershockley@uchicago.edu>
2018-04-18 08:47:14 -0500

    Fix some of the stats issues, begin massive cleanup

--- analyze.py
+++ analyze.py
@@ -27,19 +27,22 @@ class SPE:
         self.data = data.copy()
         
         # numpy magic
-        val_to_check = [4, 5, 6, 7, 8, 9, 10]
+        val_to_check = [5, 6, 7, 8]
         big_array = np.ones((248, len(data['bin_centers']), len(val_to_check)))
         occupancy_array = np.ones((248, len(val_to_check)))
             
         for i, val in enumerate(val_to_check):
             corr, sigma_corr=self.make_correction(val, 'amplitude')
-            big_array[: , : , i] = self.acceptance(val, 'amplitude')
+            big_array[: , : , i] = self.acceptance(val, 'amplitude', errors=False)
             occupancy_array[:,i] = -1*np.log(corr)
         # with systematics
         self.big_array = big_array
         self.occupancy_by_channel = (np.mean(occupancy_array, axis=1), np.std(occupancy_array, axis=1))
         self.off_channels = np.where(self.occupancy_by_channel[0] < 0.05)[0]
-        self.acceptance_by_channel = (np.mean(big_array, axis=2), np.std(big_array, axis=2))
+        systematics =  np.std(big_array, axis=2)
+        accs, statistical = self.acceptance(6)
+        self.acceptance_by_channel = accs, statistical, systematics
+        
 
     def make_correction(self, val2corr2, space):
         if space not in ['amplitude', 'charge']:
@@ -77,58 +80,48 @@ class SPE:
        
         return res, sigma_res
      
-    #make lots of acc curves using numpy magic
-    def acc_MC(self, residual, sigma_res, total_curves):
-        new_res=np.zeros((total_curves, len(residual)), dtype=float)
-        new_accs=np.zeros((total_curves, len(residual)), dtype=float)
-        samples=np.zeros((len(residual), total_curves), dtype=float)
-        for b, res in enumerate(residual):
-            mu=res
-            sigma=abs(sigma_res[b])
-            if np.isnan(sigma):
-                new_res[:,b]=mu
-            else:
-                sample=norm.rvs(loc=mu, scale=sigma, size=total_curves)
-                samples[b]=sample
-           
-            new_res=np.transpose(samples)
-            
-        new_accs =  1 - new_res.cumsum(axis=1) / new_res.sum(axis=1)[:, np.newaxis]
-        #clip to prevent unphysical values
-        new_acc=np.clip(new_accs, -0.1, 1.1)
-                     
-        return new_acc
+    def acceptance(self, val2corr2, space='amplitude', errors=True):
+        residual, residual_err = self.residual(val2corr2, space)
+        acc = A_func(residual)
         
-
-    def acceptance(self, val2corr2, space='amplitude'):
-        residual, sigma_res = self.residual(val2corr2, space)
-        acc =  1 - residual.cumsum(axis=1) / residual.sum(axis=1)[:, np.newaxis]
-        acc=np.clip(acc, -0.1, 1.1)
+        if errors:
+            minus1 = np.zeros_like(residual)
+            plus1 = np.zeros_like(minus1)
+            
+            for ch in np.arange(248):
+                MCs = acc_MC(residual[ch], residual_err[ch], 100)
+                
+                minus1[ch, :] = np.percentile(MCs, 16, axis=0)
+                plus1[ch, :] = np.percentile(MCs, 84, axis=0)
+                    
+            errors = np.array([minus1, plus1])
+            return acc, errors
         return acc
 
-    #use MC to find statistical errors
-    
-    def MC_errors(self, run_number):
-        path = os.path.join(data_dir_base, 'run_%05d.h5' % run_number)
-        if not os.path.exists(path):
-            print("Acceptance data does not exist for run %d" % run_number)
-        #initialize error arrays
-        sigma_l=np.zeros((248, len(s.data['bin_centers'])))
-        sigma_u=np.zeros((248, len(s.data['bin_centers'])))
-        ch_index = np.arange(248)
-        res, sigma_res=self.residual(6, 'amplitude')
-        #loop over channels
-        for ch in ch_index:
-            #make MC acc curves
-            acc_curves=self.acc_MC(res[ch], sigma_res[ch], 1000)
+def A_func(residual):
+    return 1 - residual.cumsum(axis=1) / residual.sum(axis=1)[:, np.newaxis]
+
+def acc_MC(residual, sigma_res, total_curves):
+    new_res=np.zeros((total_curves, len(residual)), dtype=float)
+    new_accs=np.zeros((total_curves, len(residual)), dtype=float)
+    samples=np.zeros((len(residual), total_curves), dtype=float)
+    for b, res in enumerate(residual):
+        mu=res
+        sigma=abs(sigma_res[b])
+        if np.isnan(sigma):
+            new_res[:,b]=mu
+        else:
+            sample=norm.rvs(loc=mu, scale=sigma, size=total_curves)
+            samples[b]=sample
             
-            #stats errors from MC
-            sigma_l[ch,:] = np.percentile(acc_curves,16, axis=0)-np.mean(acc_curves, axis=0)
-            sigma_u[ch,:] = np.percentile(acc_curves, 84, axis=0)-np.mean(acc_curves, axis=0)
-        stat_errs = np.array([sigma_l, sigma_u])
-       
-        return stat_errs
+        new_res=np.transpose(samples)
+            
+    new_accs =  A_func(new_res)
+    #clip to prevent unphysical values
+    new_acc=np.clip(new_accs, -0.1, 1.1)
     
+    return new_acc
+                                                                                                                                                            
 class ch_data:
     def __init__(self, runlist, date, acc, on_acc, acc_errs_l, acc_errs_u, acc_sys, acc_stat, occ, on_occ, occ_sys, occ_stat):
         self.runlist=runlist
@@ -190,48 +183,33 @@ def acceptance_fraction(run_number, thresholds):
     if not os.path.exists(path):
         print("Acceptance data does not exist for run %d" % run_number)
     s = SPE(path)
-    frac_array = np.ones((248, s.big_array.shape[-1]))
-    ch_index = np.arange(248)
     thresholds = np.array(thresholds)[:248]
     bin0 = np.where(s.data['bin_centers'] == 0.5)[0][0]
-    for i in range(s.big_array.shape[-1]):
-        frac_array[:,i] = s.big_array[...,i][ch_index, thresholds + bin0]
-        
-    acc_frac = np.mean(frac_array, axis=1)
-    sys_errs = np.std(frac_array, axis=1)
-    
-    acc_frac[s.off_channels] = 0
-    sys_errs[s.off_channels] = 0
-    stat_errs = s.MC_errors(run_number)
-    acc_errs_l=np.zeros((248, len(s.data['bin_centers'])))
-    acc_errs_u=np.zeros((248, len(s.data['bin_centers'])))
-    #propagate stats errors with sys errors
-    for ch in ch_index:
-        acc_errs_l[ch]=np.sqrt(np.mean(stat_errs[0][ch]**2)+sys_errs[ch]**2)
-        acc_errs_u[ch]=np.sqrt(np.mean(stat_errs[1][ch]**2)+sys_errs[ch]**2)
-    acc_errs=np.array([acc_errs_l, acc_errs_u])
-    
-    return acc_frac, acc_errs, sys_errs, stat_errs
+
+    acc, stat, sys = s.acceptance_by_channel
+    index = np.arange(len(acc)), bin0+thresholds
+    acc_frac = acc[index]
+    stat_frac = np.absolute(acc_frac - np.array([stat[0, index[0], index[1]], stat[1, index[0], index[1]]]))
+    sys_frac = sys[index] 
+    return acc_frac, stat_frac, sys_frac
 
 def acceptance_3runs(bottom_run, topbulk_run, topring_run, thresholds):
     thresholds = np.array(thresholds)[:248]
-    ret_acc, ret_errs_l, ret_errs_u = np.ones(248), np.ones((248, 1099)), np.ones((248,1099))
+    ret_acc, ret_stat_errs, ret_sys_errs = np.ones(248), np.ones((2, 248)), np.ones(248)
     run_list = [bottom_run, topbulk_run, topring_run]
-
+    
     channel_lists = [channel_dict['bottom_channels'],
                      channel_dict['top_bulk'],
                      channel_dict['top_outer_ring']]
-    
-    for run, ch_list in zip(run_list, channel_lists):
-        frac, acc_errs, sys_errs, stat_errs = acceptance_fraction(run, thresholds)
-        ret_acc[ch_list] = frac[ch_list]
-        ret_errs_l[ch_list] = acc_errs[0][ch_list]
-        ret_errs_u[ch_list] = acc_errs[1][ch_list]
-        
-        
-    ret_errs=[ret_errs_l, ret_errs_u]
-    return ret_acc, ret_errs, sys_errs, stat_errs
 
+    for run, ch_list in zip(run_list, channel_lists):
+        frac,  stat_errs, sys_errs = acceptance_fraction(run, thresholds)
+        ret_acc[ch_list] = frac[ch_list]
+        ret_stat_errs[:, ch_list] = stat_errs[:, ch_list]
+        ret_sys_errs[ch_list] = sys_errs[ch_list]
+        
+    return ret_acc, ret_stat_errs, ret_sys_errs
+    
 def acceptance_curve_3runs(bottom_run, topbulk_run, topring_run):
     ret_acc, ret_errs= np.ones((248, 1099)), np.ones((248, 1099))
     run_list = [bottom_run, topbulk_run, topring_run]
@@ -274,73 +252,9 @@ def occupancy_3runs(bottom_run, topbulk_run, topring_run):
         ret_stat_errs[ch_list] = occ_stat[ch_list]
     return ret_occ, ret_sys_errs, ret_stat_errs
 
-def plot_channel(ch, run_number, xlims, ylims = (-100, 500), filedir = ''):
-    return
-    # plots LED, noise, residual spectrum, acceptance as function of amplitude
-    data_dir = data_dir_base + "run_" + str(run_number)
-    file_str = data_dir + "/histograms.csv"
-    amplitudes, LED_window, LED_err = get_data_array(run_number, "LED", errors=True)
-    amplitudes, noise_window, noise_err = get_data_array(run_number, "NOISE", errors=True)
-    amplitudes, residual, res_err = get_data_array(run_number, "residual", errors=True)
-
-    plt.figure(figsize=(10,8))
-    plt.errorbar(amplitudes, LED_window[ch], yerr=LED_err[0], color='red', linestyle='none',
-                 marker='.', label='LED window')
-    plt.errorbar(amplitudes, noise_window[ch], yerr=noise_err[0], color='black', linestyle='none',
-                 marker='.', label='noise window after correction')
-    plt.yscale('log')
-    plt.xlabel('amplitude [ADC counts]')
-    plt.ylabel('counts')
-    plt.legend(loc='upper right', frameon=False)
-    plt.xlim(xlims)
-    plt.ylim(ylims)
-    plt.grid(b=True, which='both')
-    plt.title("Channel %d" % ch)
-    if filedir != '':
-        plt.savefig("%s/ch%d_LEDnoise.png" % (filedir, ch))
-
-    fig, ax1 = plt.subplots(figsize=(10,8))
-    ax1.errorbar(amplitudes, residual[ch], yerr=res_err[0], color='blue', linestyle='none')
-    ax1.set_yscale('linear')
-    ax1.set_xlabel('amplitude [ADC counts]')
-    ax1.set_ylabel('LED - noise residual [counts]')
-    ax1.set_xlim(xlims)
-    plt.title('LED - noise window residual channel %d'% ch)
-    plt.grid(b=True, which='both')
-    if filedir != '':
-        plt.savefig("%s/ch%d_LEDnoiseresidual.png" % (filedir, ch))
-
-    ax2b = ax1.twinx()
-    ax2b.set_xlim(xlims)
-    ax2b.plot(amplitudes, 1 - (np.cumsum(residual[ch]) / residual[ch].sum()),
-              color='red', linewidth=2, linestyle='steps-post')
-    ax2b.set_ylabel('Acceptance fraction')
-    ax2b.yaxis.label.set_color('red')
-    ax1.yaxis.label.set_color('blue')
-    for tl in ax1.get_yticklabels():
-        tl.set_color('b')
-    for tl in ax2b.get_yticklabels():
-        tl.set_color('r')
-    if filedir != '':
-        plt.savefig("%s/ch%d_LEDnoiseresidualACC.png" % (filedir, ch))
-    plt.show()
-
 
 def twoplus_contribution(occ):
     return 1 - np.exp(-occ)*(1+occ)
-
-
-def plot_acceptances(acceptances, output_file):
-    # takes a list of acceptances, saves png to output_file
-
-    pmtsizeArray = 700. * np.ones(len(acceptances))
-    plot_on_pmt_arrays(color=acceptances,
-                       size=pmtsizeArray,
-                       geometry='physical',
-                       colorbar_kwargs=dict(label='spe acceptance fraction'),
-                       scatter_kwargs=dict(vmin=0.5, vmax=1))
-    plt.suptitle('SPE acceptance fraction', fontsize=20)
-    plt.savefig(output_file)
 
     
 def find_threshold(bin_centers, acceptance, acc_frac):
